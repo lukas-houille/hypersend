690e4224c151696ff6fb268cb3fdb4fde78e65c6
lukashouille <lukas.houille@viacesi.fr>
2025-06-19 18:24:37 +0200

    refactor: remove unused authorizedAccess function from auth routes

--- backend/services/user-auth/app/routes/auth.routes.js
+++ backend/services/user-auth/app/routes/auth.routes.js
@@ -1,12 +1,8 @@
 import express from "express";
 import {signUp, signIn} from "../controllers/auth.controller.js";
-import {verifySignUp, authJwt} from "../middlewares/index.js";
+import {verifySignUp} from "../middlewares/index.js";
 
 const router = express.Router();
-
-const authorizedAccess = (req, res) => {
-    res.status(200).json({ message: "Authentication OK" });
-};
 
 // Signup Client Route
 router.post(
